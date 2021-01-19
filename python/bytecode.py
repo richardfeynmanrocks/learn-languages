68430940f24733854463ed42e323d18b7e69b58e
David Freifeld <freifeld.david@gmail.com>
2021-01-19 14:26:19 -0800

    Successful rewriting of bytecode

--- python/bytecode.py
+++ python/bytecode.py
@@ -1,31 +1,51 @@
+import types
+from dis import dis
+
 def test():
+    print("Testeroo")
     file = open("bytecode.py")
     a = file.read()
     c = compile(a, '<string>', 'exec')
-    fn_code = c.co_consts[0] # Pick up the function code from the main code
+    fn_code = c.co_consts[5] # Pick up the function code from the main code
+    test = c.co_consts[7] # Pick up the function code from the main code
     print (c.co_consts)
-    from dis import dis
+    print (c.co_code)
     dis(fn_code)
+    print(fn_code.co_code)
+    b = write_to_bytecode(c, fn_code, fn_code.co_code[:-14]+bytes("\x01\x00d\x00S\x00", 'utf-8'))
+    print(b.co_code)
+    dis(b)
+    dis(test)
+    print(test.co_code)
+    eval(b)
 
-def write_to_bytecode(code):
-    return types.CodeType(fn_code.co_argcount,
-                          c.co_kwonlyargcount,  # Add this in Python3
-                          c.co_posonlyargcount, # Add this in Python 3.8+
+def abc():
+    print ("hi")
+    print ("bye")
+    return 2
+
+def abcd():
+    print ("AAA")
+    
+def write_to_bytecode(code, fn_code, new_bytes):
+    updt = types.CodeType(fn_code.co_argcount,
+                          code.co_kwonlyargcount,
+                          code.co_posonlyargcount,
                           fn_code.co_nlocals,
                           fn_code.co_stacksize,
                           fn_code.co_flags,
-                          x,  # fn_code.co_code: this you changed
+                          new_bytes,
                           fn_code.co_consts,
                           fn_code.co_names,
                           fn_code.co_varnames,
                           fn_code.co_filename,
                           fn_code.co_name,
                           fn_code.co_firstlineno,
-                          fn_code.co_lnotab,   # In general, You should adjust this
+                          fn_code.co_lnotab,
                           fn_code.co_freevars,
                           fn_code.co_cellvars)
-                          
-    
+    return updt
+
 test()
 # print("=" * 30)
 
