3952347a1017b43ce480b98e656f16428e809f48
David Freifeld <freifeld.david@gmail.com>
2021-01-19 18:00:35 -0800

    Cleaned bytecode script up

--- python/bytecode.py
+++ python/bytecode.py
@@ -5,25 +5,12 @@ import math
 def test():
     c = compile(open("bytecode.py").read(), '<string>', 'exec')
     fn_code = c.co_consts[5]
-    ex_code = c.co_consts[7]
-    dis(fn_code)
     bytecode = fn_code.co_code
-    print(bytecode[56:68])
-    print(bytecode)
     bytecode = replace_op(bytecode, 4, "\x13")
     bytecode = replace_op(bytecode, 54, "\x18")
     bytecode = insert_op(bytecode, 68, "\x01|\x00|\x00d\x03k\x02r\x56|\x01S\x00a")
-    #bytecode = replace_op(bytecode, 100, "")
-    bytecode = insert_op(bytecode, 112, "\x00d\x01t\x01\x14\x00\x1b")
-    print(bytecode)
-    print(bytecode[112:114])
-    print("="*30)
-    dis(ex_code)
-    print(ex_code.co_code)
-    # bytecode[3] = 
+    bytecode = insert_op(bytecode, 112, "\x00d\x01t\x01\x14\x00\x1b")    
     b = write_to_bytecode(c, fn_code, bytecode)
-    dis(b)
-    print("Test")
     print(eval(b, {'a':2, 'b':1, 'c':3, 'sqrt':math.sqrt}))
     print(eval(b, {'a':2, 'b':1, 'c':0.125, 'sqrt':math.sqrt}))
     print(eval(b, {'a':2, 'b':1, 'c':0, 'sqrt':math.sqrt}))
@@ -32,22 +19,13 @@ def roots():
     d = b*2 - (4*a*c)
     if d < 0: return None
     r_1 = (-b+sqrt(b**2 + 4*a*c))/(2*a)
-    #if d == 0: return r_1
     r_2 = (-b-sqrt(b**2 - 4*a*c))
     return (r_1, r_2)
-
-def ex():
-    return 1/(2*a)
 
 def replace_op(bytecode, index, new):
     return bytecode[0:index] + bytes(new, "utf-8") + bytecode[index+1:]
 
 def insert_op(bytecode, index, new):
-    print ("called with", index)
-    print (bytecode[0:index+1])
-    print (bytes(new, "utf-8"))
-    print (bytecode[index+1:])
-    print ("")
     return bytecode[0:index+1] + bytes(new, "utf-8") + bytecode[index+1:]
     
 def write_to_bytecode(code, fn_code, new_bytes):
